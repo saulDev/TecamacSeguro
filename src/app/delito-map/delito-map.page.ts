bd477bd8782c0e4fb8256b493b34f05a2ba609a7
Saul <saul@moobky.com>
2019-05-03 08:50:22 -0500

    Creación de página

---
+++ src/app/delito-map/delito-map.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-delito-map',
+  templateUrl: './delito-map.page.html',
+  styleUrls: ['./delito-map.page.scss'],
+})
+export class DelitoMapPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/tab3/tab3.module.ts
+++ src/app/tab3/tab3.module.ts
@@ -1,16 +1,32 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab3Page } from './tab3.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: Tab3Page
+      },
+      {
+        path: 'delito-map',
+        loadChildren: '../delito-map/delito-map.module#DelitoMapPageModule'
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab3Page }])
+    RouterModule.forChild(routes)
   ],
   declarations: [Tab3Page]
 })
