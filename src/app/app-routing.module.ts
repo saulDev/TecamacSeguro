16b49f7ee3f7d4f75ac73e015fc358fd9b1fb21a
Saul <saul@moobky.com>
2019-06-14 00:20:32 -0500

    pantalla login completa

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
-  { path: 'modal-employee-data', loadChildren: './modal-employee-data/modal-employee-data.module#ModalEmployeeDataPageModule' },  { path: 'modal-place-data', loadChildren: './modal-place-data/modal-place-data.module#ModalPlaceDataPageModule' },
+  { path: 'app', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'modal-employee-data', loadChildren: './modal-employee-data/modal-employee-data.module#ModalEmployeeDataPageModule' },
+  { path: 'modal-place-data', loadChildren: './modal-place-data/modal-place-data.module#ModalPlaceDataPageModule' },
   { path: 'patrol-delivery', loadChildren: './patrol-delivery/patrol-delivery.module#PatrolDeliveryPageModule' },
   { path: 'visit-route', loadChildren: './visit-route/visit-route.module#VisitRoutePageModule' },
   { path: 'delito-map', loadChildren: './delito-map/delito-map.module#DelitoMapPageModule' },
-  { path: 'quadrants-all', loadChildren: './quadrants-all/quadrants-all.module#QuadrantsAllPageModule' }
+  { path: 'quadrants-all', loadChildren: './quadrants-all/quadrants-all.module#QuadrantsAllPageModule' },
+  { path: '', loadChildren: './login/login.module#LoginPageModule' }
 
 ];
 @NgModule({
---
+++ src/app/login/login.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(private navCtrl: NavController) { }
+
+  ngOnInit() {
+  }
+
+  doLogin() {
+    this.navCtrl.navigateRoot('/app/tabs');
+  }
+}
+
